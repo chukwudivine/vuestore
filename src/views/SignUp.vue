03e6b53632e1c9648cc0a7ed1bb5a34fcf49dd8c
Chukwu Divine <68476454+chukwudivine@users.noreply.github.com>
2022-09-25 22:11:20 +0100

    Update SignUp.vue

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -20,7 +20,7 @@
                         </div>
                         <div>
                             <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                            <input type="confirm-password" v-model="password2" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                            <input type="password" v-model="password2" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
                         </div>
                         <div class="bg-red-600 rounded text-center" v-if="errors.length">
                             <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
