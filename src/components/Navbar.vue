4b14b7728d172b46dd9c1c4eb8dbc129b3f02d3f
Chukwu Divine <68476454+chukwudivine@users.noreply.github.com>
2023-08-27 12:03:15 +0100

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
 
 
 <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
-    <div class="container flex flex-wrap justify-between items-center mx-auto">
+    <div class="flex flex-wrap justify-between items-center mx-auto">
     <a href="/" class="flex items-center">
         <img src="../assets/Logo.png" class="mr-3 h-10 sm:h-9" alt="Logo">
         <span class="text-orange-500 self-center text-xl font-semibold whitespace-nowrap dark:text-white">Diveck Stores</span>
